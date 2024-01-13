4226432ccd2239d01e2de37ca17c467c2dc9e507
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2024-01-13 12:07:23 +0530

    Update Practise_file.java

--- src/CodeForces_800/Practise_file.java
+++ src/CodeForces_800/Practise_file.java
@@ -3,5 +3,6 @@ package CodeForces_800;
 public class Practise_file {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello");
     }
 }
